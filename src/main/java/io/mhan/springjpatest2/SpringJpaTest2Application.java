236043b71e281d879f37b5941eaaf931784bcccc
audgks5551 <audgsk5551@daum.net>
2023-05-10 19:14:04 +0900

    init

---
+++ src/main/java/io/mhan/springjpatest2/SpringJpaTest2Application.java
@@ -0,0 +1,13 @@
+package io.mhan.springjpatest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/mhan/springjpatest2/SpringJpaTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mhan.springjpatest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
