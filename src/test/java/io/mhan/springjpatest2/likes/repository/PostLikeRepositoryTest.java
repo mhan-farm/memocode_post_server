5d5c54a24be171a41e627e136be8074dcaa22941
audgks5551 <audgsk5551@daum.net>
2023-05-11 22:09:25 +0900

    t2 문제 제출

--- src/test/java/io/mhan/springjpatest2/likes/repository/PostLikeRepositoryTest.java
+++ src/test/java/io/mhan/springjpatest2/likes/repository/PostLikeRepositoryTest.java
@@ -2,10 +2,14 @@ package io.mhan.springjpatest2.likes.repository;
 
 import io.mhan.springjpatest2.base.RepositoryTestBase;
 import io.mhan.springjpatest2.likes.entity.PostLike;
+import io.mhan.springjpatest2.posts.entity.Post;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,5 +23,20 @@ public class PostLikeRepositoryTest extends RepositoryTestBase {
         boolean result = postLikes.stream()
                 .allMatch(postLike -> postLike.getUser().getId().equals(user1.getId()));
         assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("user1가 좋아한 post 조회 and 최신순 조회")
+    void t2() {
+        List<PostLike> postLikes = postLikeRepository.findByUserId(user1.getId());
+
+        List<Post> posts = postLikes.stream()
+                .map(PostLike::getPost)
+                .collect(Collectors.toList());
+
+        boolean result = postLikes.stream()
+                .allMatch(postLike -> postLike.getUser().getId().equals(user1.getId()));
+        assertThat(result).isTrue();
+        assertThat(posts).isSortedAccordingTo(Comparator.comparing(Post::getCreated, Comparator.reverseOrder()));
     }
 }
