ed64bc4671974b1b4e4c1bf64e44e2bca3459bcb
audgks5551 <audgsk5551@daum.net>
2023-05-14 00:01:19 +0900

    findById와 findByUsername의 차이점 테스트

--- src/main/java/io/mhan/springjpatest2/users/repository/UserRepository.java
+++ src/main/java/io/mhan/springjpatest2/users/repository/UserRepository.java
@@ -4,10 +4,13 @@ import io.mhan.springjpatest2.users.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findAll();
 
     List<User> findByIdIn(List<Long> ids);
+
+    Optional<User> findByUsername(String username);
 }
--- src/test/java/io/mhan/springjpatest2/posts/repository/PostRepositoryFindAllTest.java
+++ src/test/java/io/mhan/springjpatest2/posts/repository/PostRepositoryFindAllTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 @Transactional
 @SpringBootTest
 @TestInstance(PER_CLASS)
-public class PostRepositoryTest {
+public class PostRepositoryFindAllTest {
 
     @Autowired
     TestService testService;
